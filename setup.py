7b1852b7e220c008294fd9e1a8dea5b78a1aa7c8
Russell Kyle <russell.j.kyle@gmail.com>
2016-09-07 12:27:19 -0500

    Bumped version number to match Django version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_sidebars',
-    version='0.1',
+    version='1.10',
     packages=find_packages(),
     include_package_data=True,
     license='zlib/libpng License',
