759f38786dd0ede5ea6f6ca1745493f46c06c6c1
Russell Kyle <russell.j.kyle@gmail.com>
2016-09-06 20:10:41 -0500

    Conversion into a python package.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_sidebars',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='zlib/libpng License',
+    description='A Django app for wordpress style sidebars and widgets.',
+    url='https://github.com/russelljk/simple_sidebars',
+    author='Russell Kyle',
+    author_email='russell.j.kyle@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
